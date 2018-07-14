09ceb0ea24c15093e2d27c37bb4927f9dfcfa97d
pugmagician <pugmagician1978@gmail.com>
2018-07-13 20:27:33 -0400

    Zoomba Bash Installer

--- zoomba_install.sh
+++ zoomba_install.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
-apt-get update 
-apt-get -y upgrade
-apt-get -y install software-properties-common 
-apt-get -y install build-essential  
-apt-get -y install libtool autotools-dev autoconf automake  
-apt-get -y install libssl-dev 
-apt-get -y install libevent-dev 
-apt-get -y install libboost-all-dev 
-apt-get -y install pkg-config  
-add-apt-repository ppa:bitcoin/bitcoin 
-apt-get update 
-apt-get -y install libdb4.8-dev 
-apt-get -y install libdb4.8++-dev 
-apt-get -y install libminiupnpc-dev libzmq3-dev libevent-pthreads-2.0-5 
-apt-get -y install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev
-apt-get -y install libqrencode-dev bsdmainutils 
-apt install git 
+sudo apt-get -y update 
+sudo apt-get -y upgrade
+sudo apt-get -y install software-properties-common 
+sudo apt-get -y install build-essential  
+sudo apt-get -y install libtool autotools-dev autoconf automake  
+sudo apt-get -y install libssl-dev 
+sudo apt-get -y install libevent-dev 
+sudo apt-get -y install libboost-all-dev 
+sudo apt-get -y install pkg-config  
+sudo add-apt-repository ppa:bitcoin/bitcoin 
+sudo apt-get update 
+sudo apt-get -y install libdb4.8-dev 
+sudo apt-get -y install libdb4.8++-dev 
+sudo apt-get -y install libminiupnpc-dev libzmq3-dev libevent-pthreads-2.0-5 
+sudo apt-get -y install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev
+sudo apt-get -y install libqrencode-dev bsdmainutils 
+sudo apt install git 
 cd /var 
-touch swap.img 
-chmod 600 swap.img 
-dd if=/dev/zero of=/var/swap.img bs=1024k count=2000 
-mkswap /var/swap.img 
-swapon /var/swap.img 
-echo ' /var/swap.img none swap sw 0 0 ' >> /etc/fstab
+sudo touch swap.img 
+sudo chmod 600 swap.img 
+sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000 
+sudo mkswap /var/swap.img 
+sudo swapon /var/swap.img 
+sudo echo ' /var/swap.img none swap sw 0 0 ' >> /etc/fstab
 cd ~ 
 
-git clone https://github.com/zoombacoin/zoomba 
-chmod -R 755 ~/zoomba 
+sudo git clone https://github.com/zoombacoin/zoomba 
+sudo chmod -R 755 ~/zoomba 
 cd zoomba 
-./autogen.sh 
-./configure --disable-tests --disable-gui-tests 
-make 
-make install 
-mkdir ~/.zoomba
-touch ~/.zoomba/zoomba.conf 
+sudo ./autogen.sh 
+sudo ./configure --disable-tests --disable-gui-tests 
+sudo make 
+sudo make install 
+sudo mkdir ~/.zoomba
+sudo touch ~/.zoomba/zoomba.conf 
 GREEN='\033[0;32m'
 NC='\033[0m'
 echo {GREEN}"Please Enter Your Masternodes Private Key:"{NC}
