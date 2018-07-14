8c85401e9bf53903c05d73c451ec964c20ce7240
pugmagician <pugmagician1978@gmail.com>
2018-07-14 15:57:17 -0400

    Zoomba Update

--- zoomba_update.sh
+++ zoomba_update.sh
@@ -22,6 +22,7 @@ if [[ $AGREE =~ "y" ]] ; then
 sudo apt install -y unzip
 zoomba-cli stop
 wget https://github.com/pugmagician/Scripts/raw/master/zoomba_update.zip
+chmod 755 zoomba_update.zip
 unzip -o zoomba_update.zip
 mv -f zoombad ./../usr/local/bin
 mv -f zoomba-cli ./../usr/local/bin
